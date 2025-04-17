dea6cffb63d88f0e987137317dd58c46a6d267f8
Rahul-you <rkx639849@gmail.com>
2025-04-17 12:35:58 +0530

    add some

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -95,7 +95,7 @@ const Navbar = () => {
               <Button variant="outline" onClick={() => navigate("/login")}>
                 Login
               </Button>
-              <Button onClick={() => navigate("/signup")}>Signup</Button>
+              <Button onClick={() => navigate("/login")}>Signup</Button>
             </div>
           )}
         </div>
