0b38000c08b0fc79ba2f52dbc330a124a21468b0
Rahul-you <rkx639849@gmail.com>
2025-06-03 18:18:32 +0530

    change

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,23 +1,53 @@
+// import jwt from "jsonwebtoken";
+
+// const generateToken = (res, user, message) => {
+//   const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+//     expiresIn: "1d",
+//   });
+//   return res
+//     .status(200)
+//     .cookie("token", token, {
+//       httpOnly: true,
+//       sameSite: "strict",
+//       maxAge: 24 * 60 * 60 * 1000,
+//     }).json({
+//         success:true,
+//         message,
+//         user
+//     });
+// };
+
+
+// export default generateToken
+
+
+
+
+
+
 import jwt from "jsonwebtoken";
 
 const generateToken = (res, user, message) => {
   const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
     expiresIn: "1d",
   });
+
   return res
     .status(200)
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      secure: true, // ✅ Needed for HTTPS
+      sameSite: "None", // ✅ Allows cross-origin cookies
       maxAge: 24 * 60 * 60 * 1000,
-    }).json({
-        success:true,
-        message,
-        user
+    })
+    .json({
+      success: true,
+      message,
+      user,
     });
 };
 
+export default generateToken;
 
-export default generateToken
 
 
